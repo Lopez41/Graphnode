ebaaf33e3a34e214e3ef9b3b17982eefebe5d47b
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-07-28 16:44:02 -0500

    added addNode,addEdge and areConnected to graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -18,3 +18,29 @@ Graph::~Graph()
     }
 }
 
+GraphNode* Graph::addNode(int id, int data)
+ {
+    GraphNode* newNode = new GraphNode(id, data);
+    nodes.push_back(newNode);
+    return newNode;
+}
+
+void Graph::addEdge(GraphNode* node1, GraphNode* node2) 
+{
+    node1->addNeighbor(node2);
+    node2->addNeighbor(node1);
+}
+
+bool Graph::areConnected(const GraphNode* node1, const GraphNode* node2) const
+ {
+    const std::vector<GraphNode*>& neighbors = node1->getNeighbors();
+    bool connected = false;
+
+    for (const GraphNode* neighbor : neighbors) {
+        if (neighbor == node2) 
+        {
+            connected =true;
+        }
+    }
+    return connected;
+}
