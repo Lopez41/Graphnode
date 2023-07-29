419a706c20a0490e293d2368121472ed91e24306
Adrian Lopez <adrianlopez@Adrians-Air.attlocal.net>
2023-07-28 23:08:37 -0500

    created graph and added nodes to graph

--- main.cpp
+++ main.cpp
@@ -5,7 +5,22 @@
 *Date: 07/25/23
 *Status:in progress
 *Language: C++
-*File: main.cpp
+*File: main.h
 *******************/
 #include "main.h"
 
+int main()
+ {
+    // Create a graph
+    Graph graph;
+
+    // Adding nodes to the graph
+    GraphNode* node1 = graph.addNode(1, 100);
+    GraphNode* node2 = graph.addNode(2, 200);
+    GraphNode* node3 = graph.addNode(3, 300);
+    GraphNode* node4 = graph.addNode(4, 400);
+    GraphNode* node5 = graph.addNode(5, 500);
+
+    
+    return 0;
+}
